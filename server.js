dbb9b14fd4b5b3b657ed7e83dc248d6f8d3fd124
ryanincolor <ryan@bravelikeus.com>
2016-05-27 19:20:06 +0200

    Set view directory

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine' , 'pug');
 
 // Serves static files

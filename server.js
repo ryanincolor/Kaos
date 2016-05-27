af96a552b7697bdf427518cd25091d131544fde3
ryanincolor <ryan@bravelikeus.com>
2016-05-27 19:07:06 +0200

    Init

---
+++ server.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+
+app.set('view engine' , 'pug');
+
+// Serves static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
+var kaos = require('./routes/kaos');
+
+app.get('/', kaos.home);
+
+// Server
+app.listen(PORT, function() {
+  console.log('Listening on ' + PORT);
+});

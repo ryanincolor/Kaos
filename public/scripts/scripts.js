cff5cafc63ddf7b911b4a7254ec8f2d0f433ec03
ryanincolor <ryan@bravelikeus.com>
2016-05-28 13:11:55 +0200

    Improve nav animation

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,3 +1,21 @@
-$('.expand-nav').on('click', function(){
-  $('.navigation').toggleClass('active');
+$('.expand-site-nav').on('click', function(){
+  $('.site-nav').toggleClass('active');
+  $('.site-header').toggleClass('expanded');
 });
+
+var navLink = $('.site-nav li');
+
+var delay = 0
+
+for (var i = 0; navLink.length > i; i++) {
+
+ delay+= .05;
+
+
+  console.log(delay)
+
+  $(navLink[i]).css({
+    "animation-delay" : delay + 's'
+  });
+}
+

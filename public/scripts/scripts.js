c8c57da8afd0bb10a1d5b0303dff242dd5f6fd44
ryanincolor <ryan@bravelikeus.com>
2016-05-29 20:48:23 +0200

    Add projects

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,21 +1,45 @@
-$('.expand-site-nav').on('click', function(){
-  $('.site-nav').toggleClass('active');
-  $('.site-header').toggleClass('expanded');
-});
-
-var navLink = $('.site-nav li');
-
-var delay = 0
-
-for (var i = 0; navLink.length > i; i++) {
-
- delay+= .05;
 
 
-  console.log(delay)
 
-  $(navLink[i]).css({
-    "animation-delay" : delay + 's'
+
+function delayNavLinks() {
+  var navLink = $('.site-nav li');
+  var delay = 0
+
+  for (var i = 0; navLink.length > i; i++) {
+    delay+= .05;
+    $(navLink[i]).css({
+      "animation-delay" : delay + 's'
+    });
+  }
+}
+
+function showProjects() {
+  $('.expand-site-nav').on('click', function(){
+    $('.site-nav').toggleClass('active');
+    $('.site-header').toggleClass('expanded');
+    $('.expand-site-nav').toggleClass('close');
+    $('.projects').toggleClass('visible');
+
+    $('.site-nav .primary li a').on('click', function(e){
+      e.preventDefault();
+      var target = this.hash;
+      var $target = $(target);
+
+      $(this).addClass('active');
+
+      $('html,body').stop().animate({
+        'scrollTop': $target.offset().top
+      }, 300, 'swing', function() {
+        window.location.hash = target;
+      });
+    });
   });
 }
+
+delayNavLinks() 
+showProjects() 
+
+
+
 
